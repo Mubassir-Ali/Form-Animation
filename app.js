300a864acd2b1b7775889370dc399a9adb45fece
Mubassir-Ali <mubassir11@gmail.com>
2020-12-22 10:31:04 +0500

    finish anamated form

--- app.js
+++ app.js
@@ -5,18 +5,42 @@ function animatedForm(){
         arrow.addEventListener("click", ()=>{
             const input =arrow.previousElementSibling;
             const parent =arrow.parentElement;
-            const next = parent.nextElementSibling;
+            const nextForm = parent.nextElementSibling;
 
            // console.log(input);
 
            // check for validation
 
-           validateUser
+           if(input.type==="text" && validateUser(input)){
+               //console.log("Every thing is ok!");
+               nextSlide(parent,nextForm);
+           }else if(input.type==="email" && validateEmail(input)){
+               nextSlide(parent,nextForm);
+           }else if(input.type==='password'&& validateUser(input)){
+               nextSlide(parent,nextForm);
+           }else{
+               parent.style.animation = "shake 0.5s ease";
+           }
+            
+           // get rid of animation
+
+           parent.addEventListener("animationend", ()=>{
+               parent.style.animation='';
+           })
+
         })
     })
 
 
 }
+
+
+function nextSlide(parent, nextForm){
+    parent.classList.add("innactive");
+    parent.classList.remove("active");
+    nextForm.classList.add("active");
+}
+
 
 function validateUser(user){
     if(user.value.length <6){
@@ -27,6 +51,18 @@ function validateUser(user){
         error('rgb(87,189,130)');
         return true;
     }
+}
+
+function validateEmail(email){
+    const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(validation.test(email.value)){
+        error('rgb(87,189,130)');
+        return true;
+        
+    }else{
+        error('rgb(189, 87, 87)');
+    }
+
 }
 
 function error(color){
